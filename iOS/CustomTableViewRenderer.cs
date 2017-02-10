39fba068547fe40bb6b3f9d01b6e75ae4fbd4c9c
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2017-02-10 14:49:16 +0530

    Added Custom Renderer to remove empty rows in iOS TableView.

---
+++ iOS/CustomTableViewRenderer.cs
@@ -0,0 +1,30 @@
+﻿//
+// TableViewRenderer.cs
+//
+// Author: Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
+//
+// Copyright (c) 2017 (c) Dheeraj Kumar Gunti
+//
+using System;
+using CoreGraphics;
+using SampleForms.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomTableViewRenderer))]
+namespace SampleForms.iOS
+{
+	public class CustomTableViewRenderer : ListViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null) {
+				Control.TableFooterView = new UIView();
+			}
+		}
+	}
+}
